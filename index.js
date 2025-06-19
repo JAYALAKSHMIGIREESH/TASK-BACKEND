22ed70ea59eb74c505184513053cb0b05c2fb707
gokul muthumani <jayalakshmigireesh@gmail.com>
2025-06-19 09:30:24 +0530

    second  commit

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
